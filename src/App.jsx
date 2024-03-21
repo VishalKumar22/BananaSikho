e50e62c238623a57b4592385312e4d7b8cad7eae
VishalKumar22 <vishalr2227@gmail.com>
2024-03-22 02:43:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Favorites from "./pages/favorites/Favorites";
+import Details from "./pages/details/Details";
+
+function App() {
+  return (
+    <div>
+      <div className="max-h-screen  bg-white text-gray-600 text-lg"></div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/recipe-item/:id" element={<Details />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
