29a302ef2f91075b5ee5a6cf80c68c8dd649168d
VishalKumar22 <vishalr2227@gmail.com>
2024-03-23 00:19:23 +0530

    Add HomePage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/Context'
+import RecipeItem from '../../components/recipeitem/RecipeItem'
 const Home = () => {
+  const {recipeList, loading} = useContext(GlobalContext)
+  if(loading) return <div>Loading... please wait!</div>
+  
   return (
-    <div>Home</div>
+    <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
+      {
+        recipeList && recipeList.length > 0 ?
+        recipeList.map((item) => <RecipeItem item = {item}/>)
+        
+        : <div>
+          <p className='lg-text-4xl text-xl text-center text-black font-extrabold'>
+            Nothing to show. Please search something
+            </p>
+        </div>
+      }
+      
+
+    </div>
   )
 }
 
