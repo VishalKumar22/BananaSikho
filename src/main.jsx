131161d719a3b317fb9b7003b20c0ecbc641eec4
VishalKumar22 <vishalr2227@gmail.com>
2024-03-22 23:54:42 +0530

    Add Context Api

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import GlobalContextProvider from "./context/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <GlobalContextProvider>
+        <App />
+      </GlobalContextProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
