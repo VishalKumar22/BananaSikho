f640eeca5141375a6af8087ee768bfc6ba763b93
VishalKumar22 <vishalr2227@gmail.com>
2024-03-23 00:18:09 +0530

    Add Params Hook(dynamic routering)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context/Context";
 
 const Navbar = () => {
+  const { searchParam, setSearchParam, handleSubmit } = useContext(GlobalContext);
+ 
+
   return (
     <nav className="flex justify-between items-center py-8 mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="text-4xl font-semibold">
         <NavLink to={"/"}>BananaSikho</NavLink>
       </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
+          value={searchParam}
+          onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Enter items..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-200 focus:shadow-red-200"
         />
