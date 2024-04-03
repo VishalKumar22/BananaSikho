92e0f47fcf7088cafc13203cf122430d253ba1ac
VishalKumar22 <vishalr2227@gmail.com>
2024-04-03 11:30:28 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <div className="max-h-screen  bg-white text-gray-600 text-lg"></div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
